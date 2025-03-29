b10e7d5e7407753b56ea921a2b4ff95cc9e3305e
MatthewS-dotcom <st10489708@vcconnect.edu.za>
2025-03-29 02:42:30 +0200

    Fixes and picture added

--- app/src/main/java/za/co/varsitycollege/st10489708/mealsuggesterapp/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10489708/mealsuggesterapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package za.co.varsitycollege.st10489708.mealsuggesterapp
 
+import android.media.Image
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -21,47 +23,46 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-val timeInput = findViewById<EditText>(R.id.timeInput)
+
+        //Variables declaration
+        val timeInput = findViewById<EditText>(R.id.timeInput)
         val suggestButton = findViewById<Button>(R.id.suggestButton)
         val resetButton = findViewById<Button>(R.id.resetButton)
         val resultText = findViewById<TextView>(R.id.resultText)
+
         //Created a reset button to allow the user to easily remove text added
         resetButton.setOnClickListener {
-            timeInput.text.clear()
-            //Allows the test to be blank
-            resultText.text = ""
+            timeInput.text.clear()  //clear EditText
+            resultText.text = ""  //Reset TextView to empty
         }
 
-// without the on click listener the button would not do as it is required
+        // On click button has been used to retaliate once user has pressed it
         suggestButton.setOnClickListener {
             val timeOfDay = timeInput.text.toString().trim().lowercase()
-//figure out time of day
-            val mealSuggestion = if (timeOfDay == "breakfast") {
-                "How about eggs and bacon?"
-                //Give a recommended food items
-            }else if (timeOfDay == "light snack") {
-                "How about a protein bar?"
-            } else if (timeOfDay == "lunch") {
-                "How about a burger and fries?"
-            }else if (timeOfDay == "quick bite") {
-                "How about fruit?"
-            }else if (timeOfDay == "dinner") {
-                "How about chicken and rice?"
+            //User inputs time of day
+            val mealSuggestion = if (timeOfDay == "morning") {
+                "Breakfast: How about eggs and bacon?"
+                //Give recommended food items
+            } else if (timeOfDay == "mid-morning") {
+                "Light Snack: How about a protein bar?"
+            } else if (timeOfDay == "afternoon") {
+                "Lunch: How about a burger and fries?"
+            } else if (timeOfDay == "mid-afternoon") {
+                "Mid-afternoon: How about fruit?"
+            } else if (timeOfDay == "dinner") {
+                "Dinner: How about chicken and rice?"
                 //Have to recommend a dessert :)
-            }else if (timeOfDay == "dessert") {
-                "How about a brownie?"
-                //If anything else is input show user it is an error and only for the vals
-            // and time of day added
-            }else{
-                "Please enter 'breakfast' , 'light snack' , 'lunch' , 'quick bite' , 'dinner', or 'dessert' "
+            } else if (timeOfDay == "after dinner") {
+                "Dessert: How about a brownie?"
+                //If anything else is input show user it is an error
+            } else {
+                "Please enter 'morning' , 'mid-morning' , 'afternoon' , 'mid-afternoon' , 'dinner' , or 'after dinner' "
                 //Show recommendations based on users input
             }
             resultText.text = mealSuggestion
             //Allows text to show example and have an input
-             {
 
-                }
-            }
         }
-        }
+    }
+}
 
